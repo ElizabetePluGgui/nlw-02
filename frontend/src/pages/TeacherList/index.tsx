0b33d77a53ebe94682d4c9dddc03c6b65d94965b
Elizabete Fabri <elizabete958@gmail.com>
2022-07-23 18:26:29 -0300

    Componentes e seus Conceito de Propriedade(PROPS)

--- frontend/src/pages/TeacherList/index.tsx
+++ frontend/src/pages/TeacherList/index.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import PageHeader from "../../components/PageHeader";
 
 import "./styles.css";
 
 function TeacherList() {
   return (
-    <>
-      <h1>Teacher List</h1>
-    </>
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Estes são os proffys disponíveis.">
+        <form id="search-teachers">
+          <button type="submit">Buscar</button>
+        </form>
+      </PageHeader>
+    </div>
   );
 }
 
