6edd8ba909939d237cad225c5f17c4cc810fc0a8
Elizabete Fabri <elizabete958@gmail.com>
2022-07-24 20:26:24 -0300

    Insert 1 dado in database

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,10 @@
 import express from "express";
+import routes from "./routes";
 
 const app = express();
 
 //app.use(cors());
 app.use(express.json());
-//app.use(routes);
-
-app.get("/", (request, response) => {
-  return response.json("ACESSOU A ROTA");
-});
+app.use(routes);
 
 app.listen(3333);
