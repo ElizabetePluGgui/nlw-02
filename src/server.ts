db087ea4ac80aae5260a4ca7b65715d18338aaea
Elizabete Fabri <elizabete958@gmail.com>
2022-07-24 19:12:24 -0300

    Database Sql3 and Connection

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,11 @@ import express from "express";
 const app = express();
 
 //app.use(cors());
-//app.use(express.json());
+app.use(express.json());
 //app.use(routes);
 
-app.get("/users", (request, response) => {
-  return response.send("ACESSOU A ROTA");
+app.get("/", (request, response) => {
+  return response.json("ACESSOU A ROTA");
 });
 
 app.listen(3333);
