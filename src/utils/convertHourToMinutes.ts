53ccf24450403d1e022ce8f731cc5d754e078432
Elizabete Fabri <elizabete958@gmail.com>
2022-08-25 01:31:15 -0300

    Routes to list classes

--- src/utils/convertHourToMinutes.ts
+++ src/utils/convertHourToMinutes.ts
@@ -1,3 +1,5 @@
+// CONVERTENDO OS VALORES DE STRING PARA NUMBER NO BANCO DE DADOS
+
 export default function convertHourToMinutes(time: string) {
   const [hour, minutes] = time.split(":").map(Number);
   const timeInMinutes = hour * 60 + minutes;
